72ad3ea96c8f55ab5ceb5fb742d4d54711112c0d
PAPPU KUMAR <87602146+prajapatipappu@users.noreply.github.com>
2022-04-04 15:14:10 +0530

    Add files via upload

---
+++ sudentTest/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sudentTest
+{
+    class Student
+    {
+        int rollno;
+        string name;
+        string course;
+        //int paidfee;
+        //int totalfee;
+        //int taxreturn;
+        //int payment;
+        //int fg
+
+        // static _servicetax =12.3F;
+
+
+        public Student()
+        {
+            Console.WriteLine("Enter your rollno ");
+            rollno = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your name ");
+            name = (Console.ReadLine());
+            Console.WriteLine("Enter your course ");
+            course = (Console.ReadLine());
+            //Console.WriteLine("Enter paidfee ");
+            //paidfee = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter totalfee ");
+            //totalfee = int.Parse(Console.ReadLine());
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("rollno" + " " + "name" + " " + "course" + "  " + "totalfee" + " " + "paidfee");
+        }
+
+        //public float totalfee()
+        //{
+        //    return totalfee;
+        //    fg =(paidfee * 12.3F) / 100 
+        //}
+        //public void payment()
+        //{
+        //    taxreturn = feepaid * servicetax
+
+        //        fg = taxreturn  + feepaid
+
+
+        //}
+
+        static void Main(string[] args)
+        {
+            Student s1 = new Student();
+
+            Console.ReadKey();
+        }
+
+    }
+    //internal class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //    }
+    //}
+}
