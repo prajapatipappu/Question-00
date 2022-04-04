e18702aa6c30d137974ed0c03d7a17314165870b
PAPPU KUMAR <87602146+prajapatipappu@users.noreply.github.com>
2022-04-04 14:27:17 +0530

    Add files via upload

---
+++ vehicle/vehicle/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vehicle
+{
+    class Vehicle
+    {
+         int make;
+         int model;
+         int year;
+    }
+    public void display()
+    {
+        Console.WriteLine("enter make");
+        make = Console.ReadLine();
+        Console.WriteLine("enter model");
+        model = Console.ReadLine();
+        Console.WriteLine("enter year");
+        year = Console.ReadLine();
+
+    }
+    public void Accelerator()
+    {
+
+    }
+    public void decelerate()
+    {
+
+    }
+    public void drive()
+    {
+
+    }
+    public void start()
+    {
+
+    }
+
+
+
+
+
+
+
+    //internal class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //    }
+    //}
+}
